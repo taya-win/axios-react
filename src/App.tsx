a1ddb9b5483ae6bafdcd54a79ec6cad8e40b2b0c
taya-win <tayawin.tyw@gmail.com>
2024-11-02 12:09:40 +0630

    Fetching Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import User from "./User.ts";
+
 export default function App() {
-    return <p className="text-primary">Hello World!</p>;
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        axios.get<User[]>('http://localhost:3000/users')
+            .then(res => setUsers(res.data))
+    }, []);
+
+    return <ul>
+        {users.map((user: User) => <li key={user.id}>{user.name}</li>)}
+    </ul>;
 }
