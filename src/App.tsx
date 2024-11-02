a88f84c97aba5367b07d5a9b2c4a2bae769e57ca
taya-win <tayawin.tyw@gmail.com>
2024-11-02 12:18:12 +0630

    Handling Errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import User from "./User.ts";
 
 export default function App() {
     const [users, setUsers] = useState<User[]>([]);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-        axios.get<User[]>('http://localhost:3000/users')
+        axios.get<User[]>('http://localhost:3000/xusers')
             .then(res => setUsers(res.data))
+            .catch(err => setError(err.message));
     }, []);
 
-    return <ul>
-        {users.map((user: User) => <li key={user.id}>{user.name}</li>)}
-    </ul>;
+    return <>
+        {error && <p className="text-danger">{error}</p>}
+        <ul>
+            {users.map((user: User) => <li key={user.id}>{user.name}</li>)}
+        </ul>
+    </>;
 }
