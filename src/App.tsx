99b3e9103241818bf0bfd06593c9cf3c8b31bdd7
taya-win <tayawin.tyw@gmail.com>
2024-11-02 12:45:25 +0630

    Showing a Loading Indicator

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import User from "./User.ts";
 export default function App() {
     const [users, setUsers] = useState<User[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
+
+        setLoading(true);
         axios.get<User[]>('http://localhost:3000/users', {signal: controller.signal})
-            .then(res => setUsers(res.data))
+            .then(res => {
+                setUsers(res.data);
+                setLoading(false);
+            })
             .catch(err => {
                 if(err instanceof CanceledError) return;
                 setError(err.message);
+                setLoading(false);
             });
 
         return () => controller.abort();
@@ -20,6 +27,7 @@ export default function App() {
 
     return <>
         {error && <p className="text-danger">{error}</p>}
+        {isLoading && (<div className="spinner-border"></div>)}
         <ul>
             {users.map((user: User) => <li key={user.id}>{user.name}</li>)}
         </ul>
